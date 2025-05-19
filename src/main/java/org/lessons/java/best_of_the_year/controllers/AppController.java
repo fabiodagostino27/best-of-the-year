eef355bec99b248cc6a6b63d025bc18e258aece5
fabiodagostino27 <fabiodago27@gmail.com>
2025-05-19 15:48:35 +0200

    ADD: getBestMovies and getBestSongs

---
+++ src/main/java/org/lessons/java/best_of_the_year/controllers/AppController.java
@@ -0,0 +1,43 @@
+package org.lessons.java.best_of_the_year.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.lessons.java.best_of_the_year.classes.Movie;
+import org.lessons.java.best_of_the_year.classes.Song;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequestMapping("/")
+public class AppController {
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+
+    private List<Movie> getBestMovies() {
+        List<Movie> moviesList = new ArrayList<>();
+
+        moviesList.add(new Movie(1, "The Godfather"));
+        moviesList.add(new Movie(2, "Pulp Fiction"));
+        moviesList.add(new Movie(3, "The Dark Knight"));
+        moviesList.add(new Movie(4, "Inception"));
+        moviesList.add(new Movie(5, "Fight Club"));
+
+        return moviesList;
+    }
+
+    private List<Song> getBestSongs() {
+        List<Song> songsList = new ArrayList<>();
+
+        songsList.add(new Song(1, "Bohemian Rhapsody"));
+        songsList.add(new Song(2, "Stairway to Heaven"));
+        songsList.add(new Song(3, "Hotel California"));
+        songsList.add(new Song(4, "Imagine"));
+        songsList.add(new Song(5, "Smells Like Teen Spirit"));
+
+        return songsList;
+    }
+}
