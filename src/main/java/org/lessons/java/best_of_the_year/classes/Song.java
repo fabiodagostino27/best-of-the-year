e6673a0b3163a7afb57d1fa7a20b66220b2a0005
fabiodagostino27 <fabiodago27@gmail.com>
2025-05-19 15:41:57 +0200

    ADD: Implement Movie and Song classes with basic attributes and methods

---
+++ src/main/java/org/lessons/java/best_of_the_year/classes/Song.java
@@ -0,0 +1,24 @@
+package org.lessons.java.best_of_the_year.classes;
+
+public class Song {
+    private int id;
+    private String name;
+
+    public Song(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- src/main/java/org/lessons/java/best_of_the_year/controllers/HomeController.java
+++ src/main/java/org/lessons/java/best_of_the_year/controllers/HomeController.java
@@ -3,7 +3,6 @@ package org.lessons.java.best_of_the_year.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
